944c6a71af0f137d1391803f84c99fbce4a148b7
Nichole Go <nicholego@nicholesmacbook.attlocal.net>
2019-03-10 18:05:39 -0700

    first message

---
+++ index.js
@@ -0,0 +1,68 @@
+/*
+//alert(a)
+
+var arrayA = [1, 2, 5, 6, 10]
+
+alert(arrayA);
+arrayA.push(" NOW WE WILL HAVE A NEW ELEMENT!")
+alert(arrayA);*/
+
+/*var boo = false && true || !true && true
+alert (boo)*/
+
+function addSquares(x, y) {
+	return(x*x + y*y)
+}
+
+//alert(addSquares(2, 3))
+
+function concat(str1, str2){
+	return(str1+ str2);
+}
+
+var newstring = concat("Hello ", "World");
+
+alert(newstring);
+
+
+function triProduct(x, y, z) {
+	alert(x*y*z);
+}
+alert(triProduct(10, 11, 12));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
